26fc3c745d32e1d30651f596f12945c9d2da8b7d
xin1126 <z26319491z@gmail.com>
2022-10-30 13:02:20 +0800

    feat: server TypeScript

--- server.ts
+++ server.ts
@@ -9,14 +9,14 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-io.on('connection', (socket) => {
+io.on('connection', (socket: { on: (arg0: string, arg1: (msg: string) => void) => void; }) => {
   socket.on('chat', (msg) => {
     console.log(msg);
     io.emit('chat', msg);
   });
 });
 
-app.get('*', (req, res) => {
+app.get('*', (req: unknown, res: { sendFile: (arg0: unknown) => void; }) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
