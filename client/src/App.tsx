ff94b8af78f1f5700ddb09c76fdefe5db06c71a4
xin1126 <z26319491z@gmail.com>
2022-10-30 14:17:42 +0800

    feat: eslint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,27 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { io } from 'socket.io-client';
 import logo from './logo.svg';
 import './App.css';
-import { io } from 'socket.io-client'
-const socket = io()
+
+const socket = io();
 
 function App() {
   useEffect(() => {
     socket.emit('chat', 55555);
     socket.on('chat', (msg) => {
       console.log(msg);
-    })
-  }, [])
+    });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.tsx</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
