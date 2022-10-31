623b97ebdb982db12ce9995958d7a90e803bbd0d
xin1126 <z26319491z@gmail.com>
2022-10-31 23:59:45 +0800

    feat: room

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,47 +1,29 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { io } from 'socket.io-client';
+import { setName } from '../store/user';
 
 const socket = io();
 
 const Home: React.FC = () => {
-  const [list, setList] = useState<string[]>([]);
-  const [msg, setMsg] = useState('');
-  const renderRef = useRef(true);
+  const [playerName, setPlayerName] = useState('');
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  const handelMsg = (e: React.ChangeEvent<HTMLInputElement>) => setMsg(e.target.value);
+  const handelMsg = (e: React.ChangeEvent<HTMLInputElement>) => setPlayerName(e.target.value);
 
   const submit = () => {
-    if (!msg) return;
-    socket.emit('chat', msg);
-    setMsg('');
+    if (!playerName) return;
+    socket.emit('chat', playerName);
+    dispatch(setName(playerName));
+    setPlayerName('');
     navigate('/room');
-    console.log(list);
   };
-
-  useEffect(() => {
-    if (renderRef.current && process.env.NODE_ENV === 'development') {
-      renderRef.current = false;
-      return;
-    }
-    socket.on('chat', (text) => {
-      console.log(text);
-
-      setList((data) => [...data, text]);
-    });
-  }, []);
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <div>
-        <ul>
-          {list.map((item) => (
-            <li key={item}>
-              {item}
-            </li>
-          ))}
-        </ul>
-        <input className="border border-black" value={msg} type="text" onChange={handelMsg} />
+        <input className="border border-black" value={playerName} type="text" onChange={handelMsg} />
         <button type="button" onClick={submit}>送出</button>
       </div>
     </div>
