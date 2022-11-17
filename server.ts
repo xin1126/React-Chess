7f99fa9aa0a036b14c387b9d3b93e6431218f412
xin1126 <z26319491z@gmail.com>
2022-11-18 00:03:12 +0800

    feat: vite src react

--- server.ts
+++ server.ts
@@ -1,31 +1,30 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 
-const app = express();
-app.use(express.static(path.join(__dirname, 'client/build')));
+const app = express()
+app.use(express.static(path.join(__dirname, 'client/build')))
 
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server)
 
-io.on('connection', (socket: { on: (arg0: string, arg1: (msg: string) => void) => void; }) => {
-  socket.on('playerName', (name) => {
-    io.emit('roomPlayer', name);
-  });
+io.on(
+  'connection',
+  (socket: { on: (arg0: string, arg1: (msg: string) => void) => void }) => {
+    socket.on('playerName', (name) => {
+      io.emit('roomPlayer', name)
+    })
 
-  socket.on('roomPlayer', (room) => {
-    io.emit('playerName', room);
-  });
+    socket.on('roomPlayer', (room) => {
+      io.emit('playerName', room)
+    })
+  }
+)
 
-  socket.on('playerMsg', (msg) => {
-    io.emit('playerMsg', msg);
-  });
-});
+app.get('*', (req: unknown, res: { sendFile: (arg0: unknown) => void }) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
 
-app.get('*', (req: unknown, res: { sendFile: (arg0: unknown) => void; }) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
-
-const port = process.env.PORT || 5000;
-server.listen(port);
+const port = process.env.PORT || 5000
+server.listen(port)
