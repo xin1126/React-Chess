8464555b3fa3bc3533819f26f8e3c47de18031d0
xin1126 <z26319491z@gmail.com>
2022-12-03 18:49:21 +0800

    feat: 路由守衛

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import { HashRouter, useRoutes } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { HashRouter, useRoutes, useLocation, useNavigate } from 'react-router-dom'
 import routes from './routes'
 import Footer from '@/components/Footer'
 import Heade from '@/components/Heade'
 
+const ToHome: React.FC = () => {
+  const navigate = useNavigate()
+  useEffect(() => {
+    navigate('/')
+  }, [])
+  return <div></div>
+}
+
 const Element: React.FC = () => {
   const element = useRoutes(routes)
+
+  const location = useLocation()
+  const user = useSelector((state: User) => state)
+
+  if (location.pathname !== '/' && !user.playerName) {
+    return <ToHome />
+  }
+
   return element
 }
 
