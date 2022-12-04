fe5cec1307242857b94f3c991dc639b4ba4c5da1
xin1126 <z26319491z@gmail.com>
2022-12-04 22:14:55 +0800

    feat: 棋子

---
+++ src/lib/piece.ts
@@ -0,0 +1,132 @@
+const piece = [
+  {
+    name: '將',
+    color: 'black',
+  },
+  {
+    name: '帥',
+    color: 'red',
+  },
+  {
+    name: '士',
+    color: 'black',
+  },
+  {
+    name: '士',
+    color: 'black',
+  },
+  {
+    name: '仕',
+    color: 'red',
+  },
+  {
+    name: '仕',
+    color: 'red',
+  },
+  {
+    name: '象',
+    color: 'black',
+  },
+  {
+    name: '象',
+    color: 'black',
+  },
+  {
+    name: '相',
+    color: 'red',
+  },
+  {
+    name: '相',
+    color: 'red',
+  },
+  {
+    name: '馬',
+    color: 'black',
+  },
+  {
+    name: '馬',
+    color: 'black',
+  },
+  {
+    name: '傌',
+    color: 'red',
+  },
+  {
+    name: '傌',
+    color: 'red',
+  },
+  {
+    name: '車',
+    color: 'black',
+  },
+  {
+    name: '車',
+    color: 'black',
+  },
+  {
+    name: '俥',
+    color: 'red',
+  },
+  {
+    name: '俥',
+    color: 'red',
+  },
+  {
+    name: '砲',
+    color: 'black',
+  },
+  {
+    name: '砲',
+    color: 'black',
+  },
+  {
+    name: '炮',
+    color: 'red',
+  },
+  {
+    name: '炮',
+    color: 'red',
+  },
+  {
+    name: '卒',
+    color: 'black',
+  },
+  {
+    name: '卒',
+    color: 'black',
+  },
+  {
+    name: '卒',
+    color: 'black',
+  },
+  {
+    name: '卒',
+    color: 'black',
+  },
+  {
+    name: '卒',
+    color: 'black',
+  },
+  {
+    name: '兵',
+    color: 'red',
+  },
+  {
+    name: '兵',
+    color: 'red',
+  },
+  {
+    name: '兵',
+    color: 'red',
+  },
+  {
+    name: '兵',
+    color: 'red',
+  },
+  {
+    name: '兵',
+    color: 'red',
+  },
+]
+
+export default piece
